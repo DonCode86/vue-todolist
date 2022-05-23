051748adcb467c4452d893971b3de4728a254faa
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-23 17:32:54 +0200

    aggiunta cursor pointer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const app = new Vue({
     el: '#root',
     data: {
         newTask: "",
+        completedTasks: "",
         tasks: [{
                 text: "Fare la spesa",
                 checked: false
